7dd9348fd42874993b05decd2ae9b727783aabe6
Florian Steitz <florian@fsteitz.com>
2016-10-29 15:06:59 +0200

    Added Board.cs
    It is responsible for keeping track of all revealed tiles and also for comparing them if two of them are selected.

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+///
+/// </summary>
+public class Board : MonoBehaviour {
+
+	GameObject firstRevealedTile;
+  GameObject secondRevealedTile;
+
+  /// <summary>
+  ///
+  /// </summary>
+  public bool PairRevealed() {
+    return firstRevealedTile != null && secondRevealedTile != null;
+  }
+
+  /// <summary>
+  ///
+  /// </summary>
+  public void RegisterRevealedTile(GameObject tile) {
+    if (firstRevealedTile == null) {
+      firstRevealedTile = tile;
+    } else if (secondRevealedTile == null) {
+      secondRevealedTile = tile;
+    }
+  }
+}
