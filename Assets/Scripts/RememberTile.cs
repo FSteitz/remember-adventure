9e328687a5993cbadd4e0efe3a81f03ab92d3b73
Florian Steitz <florian@fsteitz.com>
2016-10-29 14:29:40 +0200

    Added RememberTile.cs
    It is responsible for reacting to click / touch events and for rotating a tile

---
+++ Assets/Scripts/RememberTile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///
+/// </summary>
+public class RememberTile : MonoBehaviour {
+
+  private const int REVEALED_SIDE_ANGLE = 180;
+  private const int Z_AXIS_ROTATION_SPEED = 150;
+
+	private bool clicked = false;
+  private bool revealed = false;
+
+  /// <summary>
+  ///
+  /// </summary>
+	void Update () {
+    if (clicked && !revealed) {
+      float zAngle = Mathf.Round(transform.eulerAngles.z);
+
+      if (zAngle < REVEALED_SIDE_ANGLE) {
+        transform.Rotate(0, 0, Z_AXIS_ROTATION_SPEED * Time.deltaTime);
+      } else {
+        Vector3 angles = transform.eulerAngles;
+
+        angles.z = REVEALED_SIDE_ANGLE;
+        transform.eulerAngles = angles;
+        revealed = true;
+      }
+    }
+  }
+
+  /// <summary>
+  ///
+  /// </summary>
+  void OnMouseDown() {
+    clicked = true;
+  }
+}
