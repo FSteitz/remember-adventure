8e4155d5b2b4c999074fe90e237ca76556030d42
Florian Steitz <florian@fsteitz.com>
2016-11-04 21:35:13 +0100

    Refactored tile rotation
    Moved all code related to rotating tiles to the new script 'Rotator'. The script 'RememberTile' now uses the script 'Rotator' to rotate the associated game object

---
+++ Assets/Scripts/Common/Rotator.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///
+/// </summary>
+public class Rotator : MonoBehaviour {
+
+  private const float ROTATION_SPEED = 1.5f;
+
+  public readonly Vector3 FORWARD = Vector3.forward;
+  public readonly Vector3 BACK = Vector3.back;
+
+  public bool HasStarted { get; set; }
+  public bool HasFinished { get; set; }
+
+  private Vector3 direction;
+  private Vector3 targetAngle;
+  private float maxAngle;
+
+	/// <summary>
+  ///
+  /// </summary>
+	void Start () {
+    Reset();
+	}
+
+	/// <summary>
+  ///
+  /// </summary>
+	void Update () {
+    if (HasStarted && !HasFinished) {
+      Rotate();
+    }
+	}
+
+  /// <summary>
+  ///
+  /// </summary>
+  public void Rotate(Vector3 direction, float maxAngle) {
+    this.direction = direction;
+    this.targetAngle = this.direction * maxAngle;
+    this.maxAngle = maxAngle;
+
+    HasStarted = true;
+  }
+
+  /// <summary>
+  ///
+  /// </summary>
+  public void Reset() {
+    HasFinished = false;
+    HasStarted = false;
+
+    direction = Vector3.zero;
+  }
+
+  /// <summary>
+  ///
+  /// </summary>
+  private void Rotate() {
+    Vector3 targetAngles = transform.eulerAngles + targetAngle;
+    Vector3 eulerAngles = Vector3.Lerp(transform.eulerAngles, targetAngles, ROTATION_SPEED * Time.deltaTime);
+
+    // When revealing, the z-axis increases (e.g. 19, 121, 177) and it should stop once it exceeds 180 degrees. In this
+    // case, the z-axis is set to 180.
+    //
+    // When hiding, the z-axis decreases (e.g. 177, 121, 19) and it should stop once it exceeds 0 degrees, which is a
+    // value greater than 180 degrees (e.g. 360). In this case, the z-axis is set to 0.
+    if (eulerAngles.z > maxAngle) {
+      eulerAngles.z = (direction == FORWARD) ? maxAngle : 0;
+      HasStarted = false;
+      HasFinished = true;
+    }
+
+    transform.eulerAngles = eulerAngles;
+  }
+}
--- Assets/Scripts/RememberTile.cs
+++ Assets/Scripts/RememberTile.cs
@@ -9,40 +9,30 @@ using UnityEngine;
 public class RememberTile : MonoBehaviour {
 
   private const float REVEALED_SIDE_ANGLE = 180f;
-  private const float ROTATION_SPEED = 1.5f;
-
-  private readonly Vector3 FORWARD = Vector3.forward * REVEALED_SIDE_ANGLE;
-  private readonly Vector3 BACKWARD = Vector3.back * REVEALED_SIDE_ANGLE;
 
   public Board Board { get; set; }
 
+  private Rotator rotator;
 	private bool reveal = false;
   private bool revealed = false;
-  private bool registered = false;
   private bool matched = false;
   private bool reset = false;
 
   /// <summary>
   ///
   /// </summary>
+  void Start() {
+    rotator = gameObject.GetComponent<Rotator>();
+  }
+
+  /// <summary>
+  ///
+  /// </summary>
 	void Update () {
     if (reveal && !revealed) {
-      if (!registered) {
-        Board.RegisterToggledTile(transform.gameObject);
-        registered = true;
-      }
-
-      if (Rotate(FORWARD)) {
-        Board.CheckForMatch();
-        registered = false;
-        reveal = false;
-      }
+      Reveal();
     } else if (reset && revealed) {
-      if (Rotate(BACKWARD)) {
-        Board.UnregisterToggledTile(transform.gameObject);
-        matched = false;
-        reset = false;
-      }
+      Hide();
     }
   }
 
@@ -81,23 +71,32 @@ public class RememberTile : MonoBehaviour {
   /// <summary>
   ///
   /// </summary>
-  private bool Rotate(Vector3 direction) {
-    Vector3 targetAngles = transform.eulerAngles + direction;
-    Vector3 eulerAngles = Vector3.Lerp(transform.eulerAngles, targetAngles, ROTATION_SPEED * Time.deltaTime);
-    bool reachedTarget = false;
+  private void Reveal() {
+    if (rotator.HasFinished) {
+      revealed = true;
+      reveal = false;
 
-    // When revealing, the z-axis increases (e.g. 19, 121, 177) and it should stop once it exceeds 180 degrees. In this
-    // case, the z-axis is set to 180.
-    //
-    // When hiding, the z-axis decreases (e.g. 177, 121, 19) and it should stop once it exceeds 0 degrees, which is a
-    // value greater than 180 degrees (e.g. 360). In this case, the z-axis is set to 0.
-    if (eulerAngles.z > REVEALED_SIDE_ANGLE) {
-      eulerAngles.z = (direction == FORWARD) ? REVEALED_SIDE_ANGLE : 0;
-      revealed = !revealed;
-      reachedTarget = true;
+      Board.CheckForMatch();
+      rotator.Reset();
+    } else if (!rotator.HasStarted) {
+      Board.RegisterToggledTile(transform.gameObject);
+      rotator.Rotate(rotator.FORWARD, REVEALED_SIDE_ANGLE);
     }
+  }
 
-    transform.eulerAngles = eulerAngles;
-    return reachedTarget;
+  /// <summary>
+  ///
+  /// </summary>
+  private void Hide() {
+    if (rotator.HasFinished) {
+      Board.UnregisterToggledTile(transform.gameObject);
+      rotator.Reset();
+
+      revealed = false;
+      matched = false;
+      reset = false;
+    } else if (!rotator.HasStarted) {
+      rotator.Rotate(rotator.BACK, REVEALED_SIDE_ANGLE);
+    }
   }
 }
