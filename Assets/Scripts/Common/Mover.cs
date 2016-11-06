c5eb3b9c8b5d7495bbb90d90e22438fc26ab4bdc
Florian Steitz <florian@fsteitz.com>
2016-11-06 20:30:28 +0100

    Added script Mover
    This script makes it possible to move a game object to a target position (any direction)

---
+++ Assets/Scripts/Common/Mover.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///
+/// </summary>
+public class Mover : MonoBehaviour {
+
+  private const float DefaultSpeed = 1f;
+
+  public bool IsMoving { get; set; }
+
+  public float Speed { get; set; }
+
+  private Vector3 targetPosition;
+
+  void Start() {
+    targetPosition = Vector3.zero;
+    Speed = DefaultSpeed;
+    IsMoving = false;
+  }
+
+  /// <summary>
+  ///
+  /// </summary>
+	void Update () {
+    if (IsMoving) {
+      transform.position = Vector3.MoveTowards(transform.position, targetPosition, Speed * Time.deltaTime);
+
+      if (transform.position.y == targetPosition.y) {
+        targetPosition = Vector3.zero;
+        IsMoving = false;
+      }
+    }
+	}
+
+  /// <summary>
+  ///
+  /// </summary>
+  public void MoveTo(Vector3 targetPosition) {
+    this.targetPosition = targetPosition;
+    IsMoving = true;
+  }
+}
