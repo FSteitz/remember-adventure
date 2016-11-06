4eb2bcececb44c55515decbf5f2daa59d0bd4e0c
Florian Steitz <florian@fsteitz.com>
2016-11-06 20:32:10 +0100

    Added script StartupMover
    This script leverages the script Mover to move a game object on startup to a target position

---
+++ Assets/Scripts/Common/StartupMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///
+/// </summary>
+public class StartupMover : MonoBehaviour {
+
+  public Vector3 position;
+
+  /// <summary>
+  ///
+  /// </summary>
+	void Start () {
+    gameObject.GetComponent<Mover>().MoveTo(position);
+	}
+}
