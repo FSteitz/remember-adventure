e8cfcf94f7fe6db1de425baaf42d75ee9659f19d
Florian Steitz <florian@fsteitz.com>
2016-11-12 17:31:32 +0100

    Attached StartupMover to tile prefabs
    Rather than moving the tile container game object, each tile is now moved individually. This makes it more straight forward to reposition each tile afterwards

--- Assets/Scripts/Common/StartupMover.cs
+++ Assets/Scripts/Common/StartupMover.cs
@@ -9,10 +9,26 @@ public class StartupMover : MonoBehaviour {
 
   public Vector3 position;
 
+  public bool moveToX;
+  public bool moveToY;
+  public bool moveToZ;
+
   /// <summary>
   ///
   /// </summary>
 	void Start () {
+    if (!moveToX) {
+      position.x = transform.position.x;
+    }
+
+    if (!moveToY) {
+      position.y = transform.position.y;
+    }
+
+    if (!moveToZ) {
+      position.z = transform.position.z;
+    }
+
     gameObject.GetComponent<Mover>().MoveTo(position);
 	}
 }
